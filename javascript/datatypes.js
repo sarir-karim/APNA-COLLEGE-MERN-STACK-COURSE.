266095dea1feea62b32ed6fb26114966815fdda8
sarir-karim <sarirkarim755@gmail.com>
2024-09-02 15:50:07 +0500

    learning javascript

---
+++ javascript/datatypes.js
@@ -0,0 +1,40 @@
+// In this lession we are going to learn about the datatype.
+/** 
+** we have two groups of datatype in javscript 
+
+** Primitive Datypes and Non primitive Datypes
+
+primitive datatypes
+1: Numbers
+2: Boolean
+3: String;
+4: Undefined
+5: Null
+6: Symbol
+7: Bigint
+
+**To check any varible type we can use the typeof operator "typeof variablename"
+  */
+
+var name = "sarir";
+console.log(typeof name) // string
+
+var age = 20;
+console.log(typeof age) //number
+
+var follow = true;
+console.log(typeof follow) //boolean
+
+var profession;
+console.log(typeof profession) //undefined;
+
+var courseprice = null;
+console.log(typeof courseprice) // null;
+
+var number = BigInt(23243463463434343);
+console.log(typeof number) // bigint
+
+
+
+
+
---
+++ javascript/numbers.js
@@ -0,0 +1,31 @@
+// Numbers are the simplest datatype in javascript which represent the number like 
+/*
+    Positive Numbers  21;
+    Negitive Numbers -24;
+
+    integers  23
+    floting point numbers  2.56;
+*/
+
+let a = -2;
+console.log(typeof a); // number
+
+let b = 5;
+console.log(typeof a); // number
+
+let c = 5.566;
+console.log(typeof c); // number
+
+// or let's calculate with some basic arthematic operators
+
+let add = a +b;
+console.log(add); // 3;
+
+let subtract = b - c;
+console.log(subtract); // -0.5659999999999998
+
+let multiply = a * b;
+console.log(multiply)  //-10
+
+let divide = a / b;
+console.log(divide);  //0.4
