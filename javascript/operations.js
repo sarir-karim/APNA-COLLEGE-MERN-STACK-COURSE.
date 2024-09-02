b4b018625b65cfbb4da4b97ab3d65a7541a561b5
sarir-karim <sarirkarim755@gmail.com>
2024-09-02 16:16:32 +0500

    learn operations in javascript

---
+++ javascript/operations.js
@@ -0,0 +1,45 @@
+// In javascript we can perform different types of operation 
+/**
+ * addition
+ * Subtration
+ * Multiplication
+ * Division
+ * Modulus
+ * 
+ * * */
+
+let a = 10;
+let b = 20;
+
+// addition
+
+let add = a + b  // now here a and b are operands and + operator is performing the addition opeartion on a and b varible;
+console.log(add) // 30
+
+// substration
+let subtract = a -b;
+console.log(subtract) // -10
+
+//multiplication
+let multiply = (a * b) 
+console.log(multiply) // 200
+
+//division
+let division = a /b;
+console.log(division)  //0.5;
+
+//modulo (remainder operator) the modulo operator is actually used to find the odd and even number 
+let modulo = a % b;
+console.log(modulo) // 0
+
+
+// Exponentiation (power operator) when we try to find the power of any operator like 2 power 3 ( 2*2*2) then we can use the exponention opertator
+
+let exponention = 2**3;
+console.log(exponention); // 8
+
+
+
+
+
+
