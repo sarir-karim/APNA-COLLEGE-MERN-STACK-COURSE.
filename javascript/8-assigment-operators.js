a4dd8a412f841b32e3f130d9d0663a1aeab30ca7
sarir-karim <sarirkarim755@gmail.com>
2024-09-03 16:45:54 +0500

    learn assigment operators

---
+++ javascript/8-assigment-operators.js
@@ -0,0 +1,26 @@
+// previously we have learned about the assigment operator but now we are going to study some more of it.
+/**
+ * age +=2; (age = age + 2);
+ * age -=2; (age = age -2);
+ * age *=2; (age = age *2);
+ * age /=2; (age = age /2);
+ * age %=2; (age = age %2);
+ * */ 
+
+
+let age = 2;
+age += 2;  
+console.log(age) // 4
+
+age -= 2;
+console.log(age) // 2
+
+age /= 2;
+console.log(age) //1
+
+age *=2;
+console.log(age) //2
+
+age %=2;
+console.log(age) //0
+
