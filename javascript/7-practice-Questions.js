7adce2df5669c02af310cc8824a03fd61be56998
sarir-karim <sarirkarim755@gmail.com>
2024-09-03 16:33:28 +0500

    solve practice questions

---
+++ javascript/7-practice-Questions.js
@@ -0,0 +1,33 @@
+// Practice questions
+/**
+ * 1: What is value of age after this code runs?
+ *  let age = 23;
+ *  age+2 // after two years
+ * 
+ * 2: what is the value of avg after this code runs?
+ *   let hindi = 80;
+ *   let eng = 90;
+ *   let math  = 100;
+ *   let avg = (hindi + eng + math) / 3
+ * 
+ *  *note  " = " operator is used to assign value to a varible ; it's called assigment operator
+ * */ 
+
+// answer to first question
+
+let age = 23;
+age +2; 
+console.log(age) // 23; the result will remain the same becuse we are just adding 2 to the value 23 not assigning the calculated value to the age that's why the value of age remains the same;
+
+// answer to 2nd  question.
+
+let hindi = 80;
+let english = 90;
+let math  = 100;
+let avg = (hindi + english + math) / 3  //(80+90+100)/3   = 270/3 = 90;
+console.log(avg)
+
+// so the average will be 90;
+
+
+
