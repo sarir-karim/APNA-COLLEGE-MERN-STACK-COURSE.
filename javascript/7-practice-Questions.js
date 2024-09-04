801ca8dc92316b995c8f39b8b3356bc23bccd402
sarir-karim <sarirkarim755@gmail.com>
2024-09-05 00:04:13 +0500

    solve string practice questions

--- javascript/7-practice-Questions.js
+++ javascript/7-practice-Questions.js
@@ -76,3 +76,40 @@ let age2 = 10;
 let marks  = 75 // is correct 
 let isPass = true; //T must b in small letters
 let isPass2 = true; // true must not be inside "quotes"
+
+
+// practice Questions Of String
+
+/***
+ * 1: declar your name as string and print it's length in JS
+ * 2: Declar your first name as a string and print it's first character;
+ * 3: declar your name as a string and print it's last character;
+ * 4: what is output of the  following code "apnacollege"+123
+ * 5: what are lengths of an empty string & string with single space;
+ * */
+
+//solutions
+
+//1
+let myName = "sarir karim";
+console.log(myName.length) // 11- we have used the length property to show the length of my name;
+
+//2
+let firstName = "sarir";
+console.log(firstName[0]) // s- we have used the string index to show the first character of my first name
+
+//3
+let lastName = "karim";
+console.log(lastName[4]); //m because we have  subtract one from the lenth so it showed the last string from right to left
+
+//4
+let apnacollge = "apnacollege"+123;
+console.log(apnacollge); // apnacollge123 // because string uses contactination of add strings together
+
+//5 
+let str = ''
+console.log(str.length) // index of empty string will be zero because it's initial index it set as 0
+
+let spStr = ' ';
+console.log(spStr.length); // index of a spaced string is 1 because it has one space 
+
