e574daf536255b4d5841f970d75e6f9dffc5e4d8
sarir-karim <sarirkarim755@gmail.com>
2024-09-04 23:47:23 +0500

    learn null and undefined

---
+++ javascript/15-null-undefined.js
@@ -0,0 +1,17 @@
+// null and undefied are the primitive datatype in javascript
+
+// undefined: A varible that has not been assigned a value
+// null: The null value represents the intentional absence of nay obejct value;
+
+
+//undefined example
+
+let a; // variable is declared but don't have any value
+console.log(a) // undefined;
+console.log(typeof a) // undefined
+
+// null example 
+
+let b = null; // varible is declared and have a value of null expicitly;
+console.log(b) // null
+console.log(typeof b) // object
