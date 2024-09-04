58a89cdbcad3e162a0a918d043d701314e06633c
sarir-karim <sarirkarim755@gmail.com>
2024-09-04 22:30:41 +0500

    lean boolean

---
+++ javascript/11-boolean.js
@@ -0,0 +1,24 @@
+// Boolean represents a truth value true or fllse / yes or no
+/*
+we use boolean values in such situtaion where we have to answer in two values true or false;
+*/ 
+
+let age  = 20;
+
+let isAdult  = false;
+
+console.log(isAdult); // false;
+console.log(typeof isAdult) // Boolean;
+
+// we can change type of any varible simply changing it's value;
+
+let  name = "sarir";
+console.log(typeof name) // string
+
+name = false;
+
+console.log(typeof name) // Boolean
+
+name = 45;
+
+console.log(typeof name) // number
