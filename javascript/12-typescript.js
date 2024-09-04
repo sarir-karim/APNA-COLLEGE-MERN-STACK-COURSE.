c94360b05495aa0efa7884af91108fb5f9a1b508
sarir-karim <sarirkarim755@gmail.com>
2024-09-04 22:40:23 +0500

    lean about typescript

---
+++ javascript/12-typescript.js
@@ -0,0 +1,22 @@
+// typescript is static typed language developed by microsoft
+// javascript is dynamic typed language /
+/// static typed mean one a datatype is defied with a variable it's  not allowed to chanage like we did in javascript
+// dynamic typed mean we can chanage any varible type any time
+
+
+// changing type of varible is allowed in javascript
+let age = 20;
+console.log(typeof age) // number
+
+age = true 
+console.log(typeof age) // Boolean 
+
+
+// changing datatype of varible is not allowed in typescript
+
+const name = "sarir";
+console.log(name) //  string
+
+
+name = true;
+console.log(name) // Boolea 
