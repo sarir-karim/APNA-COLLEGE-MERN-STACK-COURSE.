5a0be306cc33e7221fef58aba3ccb89fa8a39fcd
sarir-karim <sarirkarim755@gmail.com>
2024-09-04 22:22:10 +0500

    learn identifiers

--- javascript/10-identifiers.js
+++ javascript/10-identifiers.js
@@ -1,1 +1,60 @@
 // Identifier in JavaScript
+/**
+ * All JavaScript varibles must be idenfied with unique names 
+ * Names can contain letter, digits, underscores, and dollar sign 
+ * Names must begin with a letter 
+ * Names can aslo begin with $ sign.
+ * Names are case Sensitive( y and Y are different variables)
+ * Reserved words ( like JavaScript keywords) cannot be used as varibles names.
+ * */ 
+
+// examples
+
+let price = 20; // vaild name
+
+let product_name  // VALID NAME
+
+let  name1 ; // valid name
+
+let price$;  // valid name;
+
+// variable name must start with 
+
+let name;  // mean with letter
+let _name; // can start with underscore
+let $price; // can start with dollar sign;
+
+// invalid variable names
+
+//let  net weight; // invalid space is not allowed;
+//let 4name; // not allowed
+
+// names are case sensitive 
+
+// these two varibles are differennt.
+let a = 0
+let A = 0 
+
+
+// * reserved keywords are not allowed as varibles name;
+
+// let let; // not allowed;
+// let const; // not allowed
+
+//* some writing techneques
+// Camelcase
+// first word starts with small letter and second letter starts with capital letter;
+
+let fullName = "sarir karim";
+
+//snake case
+//all words are in small letters but we can use underscore in the center of each word.
+
+let  product_price = 40
+
+//PascalCase 
+// all words first letter starts with capital letter 
+
+let FullName = "Programming Language";
+
+
