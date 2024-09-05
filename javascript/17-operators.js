0fc1d99c2cdc7cefb526ca71eb88e8145f05197d
sarir-karim <sarirkarim755@gmail.com>
2024-09-05 23:51:31 +0500

    learn operators

---
+++ javascript/17-operators.js
@@ -0,0 +1,50 @@
+/*
+In javascript we have 4 types of operators
+
+1: arthematic operators (+ - * / % **)
+2: unary operator (++ --)
+3: assigment operator (= )
+*/ 
+
+let a = 10;
+let b = 5;
+
+// console.log(a + b) //15 - using + operator
+// console.log(a - b) //10- using difference operator
+// console.log(a * b) //50- using multiplication opearator
+// console.log(a / b) //2- using division opeartor
+// console.log(a % b) //0 using modulo opeartor
+// console.log(a ** b) //100000 using power opearator
+
+// console.log(a)//10
+// console.log(a++)// 11
+
+// console.log(a)//11
+// console.log(++a) //11
+
+
+// comparision opeartor
+//(=, ===, >, <, <=, >=, !=, !==,)
+
+let age = 18;
+
+// console.log(age < 18) // false ;
+// console.log(age <= 18) // ture ;
+// console.log(age > 18) // flase ;
+// console.log(age >= 18) // true ;
+// console.log(age != 18) // flase;
+// console.log(age == 18) // true;
+// console.log(age === 18) // true;
+
+
+// the == and ===; == just compares the values not the datatye but === compares datatype and 
+
+
+// COMPARISION FOR NON NUMBERS
+/*
+If we try to compare non number like a b { and symbols that what will we get in the output? 
+*/ 
+
+console.log('a'< 'b') // true. Because every key on the keyboard has a unique code called unicode 
+console.log('&'> '^') // flase 
+
