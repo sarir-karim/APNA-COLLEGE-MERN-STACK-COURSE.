e6f324787444c236bd94ee14b4e52ddc8d3c7883
sarir-karim <sarirkarim755@gmail.com>
2024-09-06 14:51:46 +0500

    learn elase if and elase

--- javascript/18-conditional-statments.js
+++ javascript/18-conditional-statments.js
@@ -56,3 +56,61 @@ else if (grade >=70 && grade >=80){
 }else if (grade <= 40){
     console.log("You Faild, and Jailed😥")
 }
+
+// with elase statement
+
+let day = 1;
+
+if(day == 1){
+    console.log("Today is Sunday")
+}else if(day ==2){
+    console.log("Today is Tuesday")
+}else if (day ==3){
+    console.log("Today is Wednesday")
+}else if (day == 4){
+    console.log("Today is Thursday")
+}else if (day == 5){
+    console.log( "Today is Friday")
+}else if (day == 6){
+    console.log("Today is Saturday")
+}else{
+    console.log("Please inter day between 1 and 6");
+}
+
+// Practice Questions 
+/*
+Create a system to calculate popcorn prices based on the size customer asked for
+if size is 'XL' price is Rs. 250
+if size is 'L' price is Rs.200
+if size is 'M' rice is Rs.100 
+if size is 'S' price is Rs.50
+*/
+
+let size = "Xl";
+
+if (size == 'Xl'){
+    console.log("Price is Rs 250")
+}else if (size == 'L'){
+    console.log("Price is Rs 200")
+}else if (size == 'M'){
+    console.log("Price is Rs 100")
+}else if (size == 'S'){
+    console.log("Price is Rs 50")
+}else {
+    console.log("Size not avaiable")
+}
+
+// Nested else if 
+
+let marks = 80;
+
+if(marks >=33){
+    console.log("Pass")
+    if(marks >=80){
+        console.log("You scored Grade: A")
+    }else {
+        console.log("You Scored Grade: F")
+    }
+}else{
+    console.log("Failed")
+}
