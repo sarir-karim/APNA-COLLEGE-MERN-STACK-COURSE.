21efc5fe7e9c0475cd13c4fa0a3bbf35c7885f7d
sarir karim <sarirkarim755@gmail.com>
2025-02-05 01:53:20 +0500

    start learning arrays

---
+++ javascript/24-arrays.js
@@ -0,0 +1,10 @@
+/*
+Array: Array is a data-structure which is used to store the data in linear formate
+we can access the data of array using index.
+*/ 
+
+let students = ["karim", "sarir", "kamran"];
+console.log(students);
+//if we want to access a specific name we can use the index of access it.
+
+console.log(students[1]);
