8fb9b62144502f1d4a38ad9f65af92111a7691a7
sarir karim <sarirkarim755@gmail.com>
2025-02-04 12:22:30 -0800

    learn new string methods like slice(), replace(), repeat()0

--- javascript/23-string-methods.js
+++ javascript/23-string-methods.js
@@ -52,8 +52,47 @@ let msg = "Hello      ";
 //STRING METHODS WITH ARGUMENTS
 //1: indexOF()
 
-let text = "ILoveCoding";
+// let text = "ILoveCoding";
 
-let result = indexOf("Love")
-console.log(result)
+// let result = indexOf("Love")
+// console.log(result)
 
+
+//Now we will learn about the methods chaining
+// let's take a senario that we have a string with a lote of space and we have to remove the space and then converting that to uppercase
+
+let word = "Hello";
+
+// let wordresult = word.trim().toUpperCase();
+// console.log(wordresult)
+
+//now the new method we can do here is the slice method. Slice method is used to slice some part from the original string and shows it in a new string.
+
+let originalString = "sarirkarim"
+//now i want to show only the word sarir. so what can i do here is. i will pass the starting index and the ending index it will return me the string from index 1 to not inlcluding the last index.
+
+console.log(originalString.slice(0, 5));//sarir
+
+// and we can aslo cut the string from the center like.
+
+console.log(originalString.slice(1, 7)); //arirka
+
+//we can aslo use -ve sign it will work like if we have given the number -5 and we have the string length of 10 it will show us the string from right to left
+
+console.log(originalString.slice(-5))
+
+//another string method called replace() is used to replace any world or letter in a string with a new word or letter.
+
+
+let str = "Hello world i am coder";
+
+let newStr = str.replace("world", "all");
+
+console.log(str)
+console.log(newStr);
+
+//repeat() is also a string method which is used to repeat a string.
+
+let msg1 = "Hello MoM"
+
+console.log(msg1.repeat(10))
