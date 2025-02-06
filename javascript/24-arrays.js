5196d2388f7b546c715e03c464ada616092ea2c1
sarir karim <sarirkarim755@gmail.com>
2025-02-06 23:54:37 +0500

    learn about array a little bit

--- javascript/24-arrays.js
+++ javascript/24-arrays.js
@@ -7,4 +7,27 @@ let students = ["karim", "sarir", "kamran"];
 console.log(students);
 //if we want to access a specific name we can use the index of access it.
 
-console.log(students[1]);
+console.log(students[1]);// sarir
+
+// creating arrys of different or same types
+
+let names = ["ali", "thomas", "amir"];
+// checking the length of the array;
+console.log(names.length) // 3
+
+//creating different types of array;
+
+let info = ["sarir", 24, "pakistan"];
+//accessig the elemtns inside the array;
+
+console.log(info[0]); // sarir;
+console.log(info[0][0])//s
+
+
+//arrays are mutable means we can change the arrays anytime we want.
+
+let books  = ['book1', 'book2', 'book3'];
+console.log(books)
+console.log(books[1] = 'Think and Grow Rich');
+console.log(books)
+
