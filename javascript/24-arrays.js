d67816aaf9376bb71516305bf0871d13cfa5189c
sarir karim <sarirkarim755@gmail.com>
2025-02-07 00:08:55 +0500

    learn basic array methods

--- javascript/24-arrays.js
+++ javascript/24-arrays.js
@@ -31,3 +31,29 @@ console.log(books)
 console.log(books[1] = 'Think and Grow Rich');
 console.log(books)
 
+/**
+ * Array Methods 
+ * Push() adds element at the end of the array
+ * Pop() removes the element from the array
+ * unshift() adds element at the start of the array
+ * shift() removes the element from the start of the array
+ */
+
+
+let followers = ['a', 'b', 'c'];
+
+let newFollower = followers.push('d');
+
+// console.log(followers);
+// console.log(newFollower);
+// console.log(followers.pop());
+
+let title = followers.unshift("Followers: ");
+
+console.log(title);
+
+let blockPerson = followers.shift();
+
+console.log(blockPerson)
+
+
