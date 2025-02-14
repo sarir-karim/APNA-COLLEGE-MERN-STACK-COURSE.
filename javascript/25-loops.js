25117ae901922aaa5b7c367be7141b7f2b850b3b
sarir karim <sarirkarim755@gmail.com>
2025-02-15 01:25:39 +0500

    learn for loop

---
+++ javascript/25-loops.js
@@ -0,0 +1,10 @@
+/**
+ * loops in javascript: loops are initially used to iterate the a peace of code in javascript.
+ * for loop
+ * for loop is used to interate over a peace of code
+ * example;
+ */
+
+for(let i = 0; i <10; i++){
+    console.log(i);
+};
