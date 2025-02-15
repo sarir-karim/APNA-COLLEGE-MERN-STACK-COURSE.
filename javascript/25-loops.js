be4c6ba14bcc43d7555d1823ced398b2b1ef49b2
sarir karim <sarirkarim755@gmail.com>
2025-02-16 00:42:04 +0500

    learn some important parts of loops

--- javascript/25-loops.js
+++ javascript/25-loops.js
@@ -5,6 +5,43 @@
  * example;
  */
 
-for(let i = 0; i <10; i++){
-    console.log(i);
-};
+// for(let i = 0; i <10; i++){
+//     console.log(i);
+// };
+
+// //runnig loop in backward.
+
+
+// for(let i=10; i >=1; i--){
+//     console.log(i)
+// }
+
+// for (let i=30; i >=5; i=i-3){
+//     console.log(i)
+// }
+
+//printing odd numbers using loops
+
+
+for(let i=1; (i<=15);i=i+2){
+    console.log(i)
+}
+
+
+//printing evern numbers from 2 to 10;
+
+for (let i =2; i <=10; i = i+2){
+    console.log(i)
+}
+
+//backward printing
+
+for (let i = 10; i >=1; i = i-2){
+    console.log(i)
+}
+
+//infinite loops. we must not write initinite loops because they will run inifinitly and browser will crash.
+
+// for( let i = 0; ; i++){
+//     console.log(i)
+// }
